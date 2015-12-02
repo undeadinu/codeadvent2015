9e4cb0977ddc18ce7a95a656488c1ed5aa3ef19a
kosamari <mariko.kosaka@gmail.com>
2015-12-02 08:42:36 -0500

    domain change

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ function notfound (req, res, next){
 }
 
 function allowCrossDomain(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://jsbin.com');
+  res.header('Access-Control-Allow-Origin', 'https://null.jsbin.com');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
